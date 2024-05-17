a05a91285116a8d2d01bd6d03eb42f8b1cca9634
Yair Kukielka <yair.kukielka@taotesting.com>
2024-05-17 13:51:59 +0200

    updated to svelte 5

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import './app.css'
-import App from './App.svelte'
+import "./app.css";
+import App from "./App.svelte";
+import { mount } from "svelte";
+function mountApp(target1) {
+  const app = mount(App, { target: document.getElementById("app") });
+  return app;
+}
+mountApp();
 
-const app = new App({
-  target: document.getElementById('app'),
-})
+// const app = new App({
+//   target: document.getElementById("app"),
+// });
 
-export default app
+export default app;
