828bd0f9c7ce7b8d9d36248cf468de0f60e59b0f
Yair Kukielka <yair.kukielka@taotesting.com>
2024-07-17 09:25:57 +0200

    added code to test to try to fix it

--- src/components/component1/Component1.spec.js
+++ src/components/component1/Component1.spec.js
@@ -1,4 +1,14 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeAll } from "vitest";
+
+/** @type { Router } */
+let router;
+
+beforeAll(async () => {
+  await import("../../../.routify/routify-init.js");
+  router = globalThis.__routify.routers[0];
+  await router.ready();
+  await router.activeRoute.get().rendered;
+});
 
 describe("my description", () => {
   it("simple test", () => {
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import routify from "@roxi/routify/vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), routify({})],
+  test: {
+    environment: "jsdom",
+    // setupFiles: ["./vitest-setup.js"],
+    // pool: "forks", // this will be the default in Vitest 2.0
+    globals: true,
+  },
 });
